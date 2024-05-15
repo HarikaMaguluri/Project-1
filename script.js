8743a417a00cd30d40f18e8d34becf63b99a6dbb
HarikaMaguluri <harikamaguluri06@gmail.com>
2024-05-15 14:16:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+let x = document.getElementById("container");
+     if (x.style.display === "none") {
+       x.style.display = "block";
+     } else {
+       x.style.display = "none";
+     }
+   }
+  function Button(){
+    document.getElementById("analytics").style.backgroundColor = "#024f00";
+    document.getElementById("analytics").style.color="white";
+  }
+  function Btn(){
+    document.getElementById("meeting").style.backgroundColor = "#024f00";
+    document.getElementById("meeting").style.color="white";
+  }
